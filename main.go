fba713d1a8b99d99d2d5c08c6574a60945f0c693
Peheliya Dhanuka Navod <140178910+peheliyanavod@users.noreply.github.com>
2026-05-05 00:19:05 +0530

    run and test the backend server with dummy data

--- main.go
+++ main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json" // New: To translate our data into JSON
 	"fmt"
 	"log"
+	"net/http"      // New: To build the web server
 
-	_ "github.com/go-sql-driver/mysql" // The MySQL Driver
+	_ "github.com/go-sql-driver/mysql"
 )
+
+// dummy data structure
+type Skill struct {
+	ID   int    `json:"id"`
+	Name string `json:"skill_name"`
+}
 
 func main() {
 	// Format: "username:password@tcp(127.0.0.1:3306)/database_name"
@@ -24,5 +32,21 @@ func main() {
 		log.Fatal("Error pinging database: ", err)
 	}
 
-	fmt.Println("Successfully connected to the MySQL Database!")
+	http.HandleFunc("/skills", func(w http.ResponseWriter, r *http.Request) {
+		
+		dummySkills := []Skill{
+			{ID: 10, Name: "Go"},
+			{ID: 20, Name: "Angular"},
+		}
+
+		// Set the content type to JSON so Angular knows how to read it
+		w.Header().Set("Content-Type", "application/json")
+		
+		// Translate our Go data into JSON and send it back
+		json.NewEncoder(w).Encode(dummySkills)
+	})
+
+	fmt.Println("Server is running on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
